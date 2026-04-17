7d5d1de963880f02a640ed864c97899db75ddaa3
precious112 <preciouskent8@gmail.com>
2026-04-17 04:49:47 +0100

    till next time

--- src/hashmap.c
+++ src/hashmap.c
@@ -20,5 +20,29 @@ unsigned long djb2(char* key){
         hash= hash*33 + *key;
         key++;
     }
+    return hash;
 
+};
+
+Hashmap create(){
+    Hashmap h;
+    h.capacity=17;
+    h.count=0;
+    h.threshold=0.75;
+    h.buckets= malloc(h.capacity);
+    return h;
+};
+
+void resize(Hashmap* h){
+    int new_capacity= malloc(h->capacity*2);
+    for(int i=0;i<=h->capacity;i++){
+
+    }
+
+    
+};
+
+void set(Hashmap* h, char* key, void* value){
+    unsigned long hash = djb2(key);
+    int index = hash % h->capacity;
 };
